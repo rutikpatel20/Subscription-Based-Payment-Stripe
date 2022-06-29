c6d604b2047fbd6d12bc647c76e28ac049909b5f
rutiksimform <rutik.p@simformsolutions.com>
2022-06-29 13:21:08 +0530

    gem faker to generate fake posts

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+10.times do
+  Post.create(
+    title: Faker::ProgrammingLanguage.name,
+    content: Faker::Quote.famous_last_words,
+    premium: [true, false].sample,
+  )
+end
