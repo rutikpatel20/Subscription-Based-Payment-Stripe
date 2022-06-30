e48d191f3ab2854cc39f1862ac4b8dd9f904f830
rutiksimform <rutik.p@simformsolutions.com>
2022-06-30 18:03:13 +0530

    link to stripe checkout session

---
+++ app/controllers/checkout_controller.rb
@@ -0,0 +1,16 @@
+class CheckoutController < ApplicationController
+  def create
+    @session = Stripe::Checkout::Session.create({
+      success_url: posts_url,
+      cancel_url: pricing_url,
+      payment_method_types: ["card"],
+      line_items: [
+        { price: "price_1LGKOXSDDkM6tNQ6AeKZG1X8", quantity: 1 },
+      ],
+      mode: "subscription",
+    })
+    respond_to do |format|
+      format.js
+    end
+  end
+end
